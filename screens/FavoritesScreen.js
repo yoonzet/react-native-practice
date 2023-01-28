f553a244f43490244985ff4ab3de4b2dd27671e0
yoonji lee <lalala703@naver.com>
2023-01-28 23:40:34 +0900

    컴포넌트 내에서 Redux 상태 사용하기

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,19 @@
 import React, {useContext} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {useSelector} from 'react-redux';
 import MealsList from '../components/MealsList/MealsList';
 import {MEALS} from '../data/dummy-data';
 import {FavoritesContext} from '../store/context/favorites-context';
 
 function FavoritesScreen() {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector(state => state.favoriteMeals.ids);
+
+  // const favoriteMeals = MEALS.filter(meal =>
+  //   favoriteMealsCtx.ids.includes(meal.id),
+  // );
   const favoriteMeals = MEALS.filter(meal =>
-    favoriteMealsCtx.ids.includes(meal.id),
+    favoriteMealIds.ids.includes(meal.id),
   );
   if (favoriteMeals.length === 0) {
     return (
